b9413279fb0de79ab3d9413035eb36f6ff30baa5
Helen <firetruk@umich.edu>
2021-08-04 14:41:20 +0900

    project started

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = 3000;
+
+// app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use(express.static(path.join(__dirname, '..')));
+
+
+app.use('/', (req, res) => {
+  res.render('index.html')
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at localhost:${port}!`);
+});
