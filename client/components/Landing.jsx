c0cf15b178748f9fc1015d4e87c9ce4482017927
Helen <firetruk@umich.edu>
2021-08-08 08:32:48 +0900

    fixed image issues - might start project over though

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
-
+import tailwindcss from 'tailwindcss';
 function Landing() {
   return (
     <div className="landing">
       <h1 className="intro">Welcome to Sleepy Study!</h1>
-      <img className="connection" src="../assets/connect.svg" alt="connection"></img>
-      <p className="intro-paragraph-1">
+      <img className="connection" src="../assets/connection.svg" alt="connection"></img>
+      <p className="intro-paragraph">
         Did you know you might be able to learn while you sleep?
         There has been multiple studies demonstarting people's ability
         to learn while they are asleep, in a basic learning form called conditioning.
       </p>
-      <p className="intro-paragraph-2">
+      <p className="intro-paragraph">
         These studies had their subjects listen to different recordings while they were under REM sleep.
         Replicating the conditions of these experiments, this application allows you to listen to
         Youtube tutorials while you are asleep.
       </p>
-      <p className="intro-paragraph-3">
-        Will this application really supplement your learning? I can't say for sure.
-        But you won't lose anything from trying it out!
-      </p>
-      <img className="studying" src="../assets/studying.svg" alt="studying"></img>
+      <div className="p3">
+        <p className="intro-paragraph">
+          Will this application really supplement your learning? I can't say for sure.
+          But you won't lose anything from trying it out!
+        </p>
+        <img className="studying" src="../assets/studying.svg" alt="studying"></img>
+      </div>
     </div>
   )
 }
