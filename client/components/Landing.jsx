9c403d508f6a6996c217d648890fb2c7633f95b9
Helen <firetruk@umich.edu>
2021-08-09 11:31:50 +0900

    what a mess i am

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import tailwindcss from 'tailwindcss';
+import { Button, Image } from "react-bootstrap";
+
 function Landing() {
   return (
     <div className="landing">
       <h1 className="intro">Welcome to Sleepy Study!</h1>
-      <img className="connection" src="../assets/connection.svg" alt="connection"></img>
+      {/* <img className="img-fluid connection" src="../assets/connection.svg" alt="connection"></img> */}
+      <Image
+          src="../assets/connection.svg"
+          roundedCircle
+      />
       <p className="intro-paragraph">
         Did you know you might be able to learn while you sleep?
         There has been multiple studies demonstarting people's ability
@@ -15,14 +20,32 @@ function Landing() {
         Replicating the conditions of these experiments, this application allows you to listen to
         Youtube tutorials while you are asleep.
       </p>
+      <Button variant="primary" className="btn-primary">
+      Large button
+    </Button>
       <div className="p3">
         <p className="intro-paragraph">
           Will this application really supplement your learning? I can't say for sure.
           But you won't lose anything from trying it out!
         </p>
-        <img className="studying" src="../assets/studying.svg" alt="studying"></img>
+        {/* <img className="img-thumbnail" src="../assets/studying.svg" alt="studying"></img> */}
+        <Image
+          src="../assets/studying.svg"
+          roundedCircle
+        />
       </div>
     </div>
+    // <Container>
+    //   <Row>
+    //     <Col>1 of 2</Col>
+    //     <Col>2 of 2</Col>
+    //   </Row>
+    //   <Row>
+    //     <Col>1 of 3</Col>
+    //     <Col>2 of 3</Col>
+    //     <Col>3 of 3</Col>
+    //   </Row>
+    // </Container>
   )
 }
 
--- client/index.jsx
+++ client/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './stylesheets/landing.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
   <App />,
