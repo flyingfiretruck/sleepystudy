ad8fa1d21e396777efa6797187134c839ac404e0
Helen <firetruk@umich.edu>
2021-08-11 10:11:02 +0900

    scroll animation sort of working

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button, Image } from "react-bootstrap";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-const Landing = () => (
-  <div className="landing">
-    <Image className="connection-image" src="../assets/connection.svg" alt="connection" />
-    <p className="animate__animated animate__bounce intro-paragraph">
-      Did you know you might be able to learn while you sleep?
-      There has been multiple studies demonstarting people's ability
-      to learn while they are asleep, in a basic learning form called conditioning.
-    </p>
-    <p className="intro-paragraph">
-      These studies had their subjects listen to different recordings while they were under REM sleep.
-      Replicating the conditions of these experiments, this application allows you to listen to
-      Youtube tutorials while you are asleep.
-    </p>
-    <div className="p3">
-      <p className="intro-paragraph">
-        Will this application really supplement your learning? I can't say for sure.
-        But you won't lose anything from trying it out!
+const Landing = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1500});
+  }, []);
+  return (
+    <div className="landing">
+      <Image className="connection-image" src="../assets/connection.svg" alt="connection" />
+      <p className="intro-paragraph" data-aos="fade-up" data-aos-anchor=".other-element">
+        Did you know you might be able to learn while you sleep?
+        There has been multiple studies demonstarting people's ability
+        to learn while they are asleep, in a basic learning form called conditioning.
       </p>
       <Image className="studying-image" src="../assets/studying.svg" alt="studying"/>
+
+      <p className="intro-paragraph" data-aos="fade-up" data-aos-anchor=".other-element">
+        These studies had their subjects listen to different recordings while they were under REM sleep.
+        Replicating the conditions of these experiments, this application allows you to listen to
+        Youtube tutorials while you are asleep.
+      </p>
+      <div className="p3">
+      <p className="intro-paragraph" data-aos="fade-up" data-aos-anchor=".other-element">
+          Will this application really supplement your learning? I can't say for sure.
+          But you won't lose anything from trying it out!
+        </p>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 
 export default Landing;
