779444337dc60a229e7892e1ef99e17ad10d821e
Helen <firetruk@umich.edu>
2021-08-11 11:40:54 +0900

    뻘짓

--- dist/bundle.js
+++ dist/bundle.js
@@ -533,7 +533,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default()));
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, ".navbar {\n  font-size: large;\n}\n.navbar-dark .navbar-brand {\n  font-size: xx-large;\n}", "",{"version":3,"sources":["webpack://./client/stylesheets/navbar.css"],"names":[],"mappings":"AAAA;EACE,gBAAgB;AAClB;AACA;EACE,mBAAmB;AACrB","sourcesContent":[".navbar {\n  font-size: large;\n}\n.navbar-dark .navbar-brand {\n  font-size: xx-large;\n}"],"sourceRoot":""}]);
+___CSS_LOADER_EXPORT___.push([module.id, ".navbar {\n  font-size: large;\n}\n.navbar-dark .navbar-brand {\n  font-size: xx-large;\n}\n#root {\n  margin: 0px;\n}", "",{"version":3,"sources":["webpack://./client/stylesheets/navbar.css"],"names":[],"mappings":"AAAA;EACE,gBAAgB;AAClB;AACA;EACE,mBAAmB;AACrB;AACA;EACE,WAAW;AACb","sourcesContent":[".navbar {\n  font-size: large;\n}\n.navbar-dark .navbar-brand {\n  font-size: xx-large;\n}\n#root {\n  margin: 0px;\n}"],"sourceRoot":""}]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
