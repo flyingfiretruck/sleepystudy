b03c989db88ae5343cc9c905ae06c54d56a0f9ec
flyingfiretruck <firetruk@umich.edu>
2021-08-19 19:20:16 +0900

    api key properly stored

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 const path = require('path');
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 const port = 3000;
 
@@ -16,7 +17,7 @@ app.use('/', (req, res) => {
 })
 
 app.get('/search', (req, res) => {
-  axios.get(`https://www.googleapis.com/youtube/v3/search?key=${keys.google}&q=${req}`)
+  axios.get(`https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&q=${req}`)
     .then((response) => {
       res.json(response.items);
     })
