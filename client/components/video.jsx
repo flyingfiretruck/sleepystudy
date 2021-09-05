b549d8a1ac9db7330e7718b1b35c136ad39a7d3a
flyingfiretruck <firetruk@umich.edu>
2021-09-06 03:12:13 +0900

    search result looks clean!

--- client/components/video.jsx
+++ client/components/video.jsx
@@ -26,55 +26,57 @@ const Video = () => {
   return (
     <div className="video">
       <div className="video-empty-space"></div>
-      <div className="video-description">
-        <p>Choose a video</p>
-        <p className="video-reminder">Remember not to click out of the website!</p>
-      </div>
-      <div className="video-search-bar">
-        <Form onSubmit={getResults}>
-          <InputGroup className="video-search">
-            <FormControl
-              className="search-bar"
-              onChange={handleChange}
-              placeholder="Search"
-              area-label="Default"
-              area-descibedby="inputGroup-sizing-default"
-            />
-            <Button className="search-button" type="submit" variant="outline-dark">
-              Search
-            </Button>
-          </InputGroup>
-        </Form>
-      </div>
-      <div className="search-results">
-        {results ?
-          results.map(result =>
-            {
-              return (
-                <div className="results-video-info">
-                  <img className="video-thumbnail"
-                    src={"https://img.youtube.com/vi/" + result['id']['videoId'] + "/mqdefault.jpg"}
-                  />
-                  <div className="video-text-info">
-                    <div className="video-title">
-                      {decode(result['snippet']['title'])}
-                    </div>
-                    <div className="video-channel">
-                      {decode(result['snippet']['channelTitle'])}
+      <div className="video-search-window">
+        <div className="video-description">
+          <p>Choose a video</p>
+          <p className="video-reminder">Remember not to click out of the website!</p>
+        </div>
+        <div className="video-search-bar">
+          <Form onSubmit={getResults}>
+            <InputGroup className="video-search">
+              <FormControl
+                className="search-bar"
+                onChange={handleChange}
+                placeholder="Search"
+                area-label="Default"
+                area-descibedby="inputGroup-sizing-default"
+              />
+              <Button className="search-button" type="submit" variant="outline-dark">
+                Search
+              </Button>
+            </InputGroup>
+          </Form>
+        </div>
+        <div className="search-results">
+          {results ?
+            results.map(result =>
+              {
+                return (
+                  <div className="results-video-info">
+                    <img className="video-thumbnail"
+                      src={"https://img.youtube.com/vi/" + result['id']['videoId'] + "/mqdefault.jpg"}
+                    />
+                    <div className="video-text-info">
+                      <div className="video-title">
+                        {decode(result['snippet']['title'])}
+                      </div>
+                      <div className="video-channel">
+                        {decode(result['snippet']['channelTitle'])}
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            }
-          )
-          : <div />
-        }
-      </div>
-      <div className="video-player">
-        <ReactPlayer url='https://youtu.be/cfD9Oz_8BwM' width="auto" height='13em'/>
-      </div>
-      <div className="playlist-no-longin">
-        Login for access to more features
+                );
+              }
+            )
+            : <div />
+          }
+        </div>
+        <div className="video-player">
+          <ReactPlayer url='https://youtu.be/cfD9Oz_8BwM' width="auto" height='13em'/>
+        </div>
+        <div className="playlist-no-longin">
+          Login for access to more features
+        </div>
       </div>
       <Image className="door-image" src="../assets/door2.svg" alt="door" />
     </div>
